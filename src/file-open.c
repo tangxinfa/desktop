000d33d360c1cb073fadd132490c64cba3e3103b
tangxinfa <tangxinfa@gmail.com>
2019-03-06 18:38:16 +0800

    fix: open file by mime type.

--- src/file-open.c
+++ src/file-open.c
@@ -1,82 +1,95 @@
+#include <libgen.h>  // for basename
 #include <limits.h>
 #include <linux/limits.h>  // for PATH_MAX
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-int console_open(const char* file) {
-  const char* program = NULL;
-  const char* extension = strrchr(file, '.');
-  const int length = strlen(file);
-
-  if (extension == NULL) {
-    return EXIT_FAILURE;
+void guess_file_extension(const char* file, char* extension,
+                          int extension_size) {
+  memset(extension, 0, extension_size);
+  char command[255] = {'\0'};
+  snprintf(command, sizeof(command), "file -b --extension %s", file);
+  FILE* f = popen(command, "r");
+  if (f != NULL) {
+    fgets(extension, extension_size, f);
+    char* end = strchr(extension, '\n');
+    if (end) {
+      *end = '\0';
+    }
+    end = strchr(extension, '/');
+    if (end) {
+      *end = '\0';
+    }
+    pclose(f);
   }
-  if (strcasecmp(extension, ".pdf") == 0) {
-    program = "fbpdf2";
-  } else if (strcasecmp(extension, ".jpg") == 0 ||
-             strcasecmp(extension, ".jpeg") == 0 ||
-             strcasecmp(extension, ".ppm") == 0 ||
-             strcasecmp(extension, ".tiff") == 0 ||
-             strcasecmp(extension, ".xwd") == 0 ||
-             strcasecmp(extension, ".bmp") == 0 ||
-             strcasecmp(extension, ".png") == 0 ||
-             strcasecmp(extension, ".webp") == 0) {
-    program = "fbi -a";
-  } else if (strcasecmp(extension, ".gif") == 0) {
-    program = "mplayer -vo fbdev2 -msglevel all=0 -fs -loop 0";
-  } else if (strcasecmp(extension, ".3g2") == 0 ||
-             strcasecmp(extension, ".3gp") == 0 ||
-             strcasecmp(extension, ".asf") == 0 ||
-             strcasecmp(extension, ".ask") == 0 ||
-             strcasecmp(extension, ".avi") == 0 ||
-             strcasecmp(extension, ".c3d") == 0 ||
-             strcasecmp(extension, ".dat") == 0 ||
-             strcasecmp(extension, ".divx") == 0 ||
-             strcasecmp(extension, ".dvr-ms") == 0 ||
-             strcasecmp(extension, ".f4v") == 0 ||
-             strcasecmp(extension, ".flc") == 0 ||
-             strcasecmp(extension, ".fli") == 0 ||
-             strcasecmp(extension, ".flv") == 0 ||
-             strcasecmp(extension, ".flx") == 0 ||
-             strcasecmp(extension, ".m2p") == 0 ||
-             strcasecmp(extension, ".m2t") == 0 ||
-             strcasecmp(extension, ".m2ts") == 0 ||
-             strcasecmp(extension, ".m2v") == 0 ||
-             strcasecmp(extension, ".m4v") == 0 ||
-             strcasecmp(extension, ".mkv") == 0 ||
-             strcasecmp(extension, ".mlv") == 0 ||
-             strcasecmp(extension, ".mov") == 0 ||
-             strcasecmp(extension, ".mp4") == 0 ||
-             strcasecmp(extension, ".mpe") == 0 ||
-             strcasecmp(extension, ".mpeg") == 0 ||
-             strcasecmp(extension, ".mpg") == 0 ||
-             strcasecmp(extension, ".mpv") == 0 ||
-             strcasecmp(extension, ".mts") == 0 ||
-             strcasecmp(extension, ".ogm") == 0 ||
-             strcasecmp(extension, ".qt") == 0 ||
-             strcasecmp(extension, ".ra") == 0 ||
-             strcasecmp(extension, ".rm") == 0 ||
-             strcasecmp(extension, ".rmvb") == 0 ||
-             strcasecmp(extension, ".swf") == 0 ||
-             strcasecmp(extension, ".tp") == 0 ||
-             strcasecmp(extension, ".trp") == 0 ||
-             strcasecmp(extension, ".ts") == 0 ||
-             strcasecmp(extension, ".uis") == 0 ||
-             strcasecmp(extension, ".uisx") == 0 ||
-             strcasecmp(extension, ".uvp") == 0 ||
-             strcasecmp(extension, ".vob") == 0 ||
-             strcasecmp(extension, ".vsp") == 0 ||
-             strcasecmp(extension, ".webm") == 0 ||
-             strcasecmp(extension, ".wmv") == 0 ||
-             strcasecmp(extension, ".wmvhd") == 0 ||
-             strcasecmp(extension, ".wtv") == 0 ||
-             strcasecmp(extension, ".xvid") == 0) {
-    program = "mplayer -vo fbdev2 -msglevel all=0 -fs";
-  } else {
-    return EXIT_FAILURE;
+}
+
+void get_file_extension(const char* file, char* extension, int extension_size) {
+  memset(extension, 0, extension_size);
+  char path[PATH_MAX] = {'\0'};
+  strncpy(path, file, sizeof(path) - 1);
+  const char* name = basename(path);
+  const char* dot = strrchr(name, '.');
+  if (dot) {
+    snprintf(extension, extension_size, "%s", dot + 1);
+  }
+}
+
+const char* get_console_program(const char* extension) {
+  if (strcasecmp(extension, "pdf") == 0) {
+    return "~/bin/pdf-viewer";
+  }
+  if (strcasecmp(extension, "jpg") == 0 || strcasecmp(extension, "jpeg") == 0 ||
+      strcasecmp(extension, "ppm") == 0 || strcasecmp(extension, "tiff") == 0 ||
+      strcasecmp(extension, "xwd") == 0 || strcasecmp(extension, "bmp") == 0 ||
+      strcasecmp(extension, "png") == 0 || strcasecmp(extension, "webp") == 0) {
+    return "fbi -a";
+  }
+  if (strcasecmp(extension, "gif") == 0) {
+    return "mplayer -vo fbdev2 -msglevel all=0 -fs -loop 0";
+  }
+  if (strcasecmp(extension, "3g2") == 0 || strcasecmp(extension, "3gp") == 0 ||
+      strcasecmp(extension, "asf") == 0 || strcasecmp(extension, "ask") == 0 ||
+      strcasecmp(extension, "avi") == 0 || strcasecmp(extension, "c3d") == 0 ||
+      strcasecmp(extension, "dat") == 0 || strcasecmp(extension, "divx") == 0 ||
+      strcasecmp(extension, "dvr-ms") == 0 ||
+      strcasecmp(extension, "f4v") == 0 || strcasecmp(extension, "flc") == 0 ||
+      strcasecmp(extension, "fli") == 0 || strcasecmp(extension, "flv") == 0 ||
+      strcasecmp(extension, "flx") == 0 || strcasecmp(extension, "m2p") == 0 ||
+      strcasecmp(extension, "m2t") == 0 || strcasecmp(extension, "m2ts") == 0 ||
+      strcasecmp(extension, "m2v") == 0 || strcasecmp(extension, "m4v") == 0 ||
+      strcasecmp(extension, "mkv") == 0 || strcasecmp(extension, "mlv") == 0 ||
+      strcasecmp(extension, "mov") == 0 || strcasecmp(extension, "mp4") == 0 ||
+      strcasecmp(extension, "mpe") == 0 || strcasecmp(extension, "mpeg") == 0 ||
+      strcasecmp(extension, "mpg") == 0 || strcasecmp(extension, "mpv") == 0 ||
+      strcasecmp(extension, "mts") == 0 || strcasecmp(extension, "ogm") == 0 ||
+      strcasecmp(extension, "qt") == 0 || strcasecmp(extension, "ra") == 0 ||
+      strcasecmp(extension, "rm") == 0 || strcasecmp(extension, "rmvb") == 0 ||
+      strcasecmp(extension, "swf") == 0 || strcasecmp(extension, "tp") == 0 ||
+      strcasecmp(extension, "trp") == 0 || strcasecmp(extension, "ts") == 0 ||
+      strcasecmp(extension, "uis") == 0 || strcasecmp(extension, "uisx") == 0 ||
+      strcasecmp(extension, "uvp") == 0 || strcasecmp(extension, "vob") == 0 ||
+      strcasecmp(extension, "vsp") == 0 || strcasecmp(extension, "webm") == 0 ||
+      strcasecmp(extension, "wmv") == 0 ||
+      strcasecmp(extension, "wmvhd") == 0 ||
+      strcasecmp(extension, "wtv") == 0 || strcasecmp(extension, "xvid") == 0) {
+    return "mplayer -vo fbdev2 -msglevel all=0 -fs";
+  }
+  return NULL;
+}
+
+int console_open(const char* file) {
+  char extension[10] = {'\0'};
+  get_file_extension(file, extension, sizeof(extension));
+  const char* program = get_console_program(extension);
+  if (program == NULL) {
+    guess_file_extension(file, extension, sizeof(extension));
+    program = get_console_program(extension);
+    if (program == NULL) {
+      return EXIT_FAILURE;
+    }
   }
 
   char command[4096] = {'\0'};
